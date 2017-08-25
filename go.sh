801198aab26b4abe850d384b55c38228ba8956eb
Justin T. Sharp <sharp@sharpone.net>
2017-08-25 13:19:57 -0600

    import

---
+++ go.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ln -nsf ~/.dotfiles/Code/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -nsf ~/.dotfiles/Code/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+ln -nsf ~/.dotfiles/Code/snippets ~/Library/Application\ Support/Code/User/snippets
